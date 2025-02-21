3856fc98f5c78ff3092693f9fd0229c49d9ce997
Joel Andersson <joelande94@gmail.com>
2025-02-21 14:37:05 +0200

    Allow giving more than one city

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 import geopy.distance
 from pyproj import Transformer
 
@@ -72,29 +73,53 @@ f = open(f"{root_path}cities.csv", "r")
 cities = [City(line) for line in f.readlines()]
 f.close()
 
-if len(sys.argv) == 3:
-    cities1 = find_city_by_name(sys.argv[1], cities)
-    city1 = validate_found_cities(sys.argv[1], cities1)
+args = sys.argv[1:]
 
-    cities2 = find_city_by_name(sys.argv[2], cities)
-    city2 = validate_found_cities(sys.argv[2], cities2)
 
-    print(f"The distance between {city1} and {city2} is: {city1.distance(city2)} km")
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
-elif len(sys.argv) == 5:
-    lat1, lon1, lat2, lon2 = map(float, sys.argv[1:5])
-    distance = distance_between_coordinates(lat1, lon1, lat2, lon2)
-    nearest_city1 = find_nearest_city(lat1, lon1, cities)
-    nearest_city2 = find_nearest_city(lat2, lon2, cities)
 
-    print(
-        f"Distance between ({lat1}, {lon1}) and ({lat2}, {lon2}) is {distance:.2f} km"
-    )
-    print(f"Nearest city to ({lat1}, {lon1}) is {nearest_city1}")
-    print(f"Nearest city to ({lat2}, {lon2}) is {nearest_city2}")
+def args_are_coordinates(args):
+    return all(is_number(arg) for arg in args)
 
+
+def args_are_cities(args):
+    return all(isinstance(arg, str) for arg in args)
+
+
+if args_are_coordinates(args):
+    try:
+        lat1, lon1, lat2, lon2 = map(float, args)
+        distance = distance_between_coordinates(lat1, lon1, lat2, lon2)
+        nearest_city1 = find_nearest_city(lat1, lon1, cities)
+        nearest_city2 = find_nearest_city(lat2, lon2, cities)
+
+        print(
+            f"Distance between ({lat1}, {lon1}) and ({lat2}, {lon2}) is {distance:.2f} km"
+        )
+        print(f"Nearest city to ({lat1}, {lon1}) is {nearest_city1}")
+        print(f"Nearest city to ({lat2}, {lon2}) is {nearest_city2}")
+    except ValueError:
+        print("Invalid coordinates. Expected four numbers.")
+        sys.exit(1)
+
+elif args_are_cities(args):
+    city_objs = []
+    for arg in args:
+        found_cities = find_city_by_name(arg, cities)
+        city_objs.append(validate_found_cities(arg, found_cities))
+
+    for city1, city2 in itertools.combinations(city_objs, 2):
+        print(
+            f"The distance between {city1} and {city2} is: {city1.distance(city2)} km"
+        )
 else:
     print(
-        "Usage: python script.py <city1> <city2> OR python script.py <lat1> <lon1> <lat2> <lon2>"
+        "Usage: python script.py <city1> <city2> ... OR python script.py <lat1> <lon1> <lat2> <lon2>"
     )
     sys.exit(1)
